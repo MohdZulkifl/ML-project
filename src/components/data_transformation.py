b493c4e8cfbec4bed3799d3543d0eef9f01bd918
MohdZulkifl <zulkiflaamir10@gmail.com>
2026-02-07 12:27:27 +0530

    Model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,24 +1,34 @@
 import sys
-from  dataclasses import dataclass
-import numpy as np
+from dataclasses import dataclass
+
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
 import os
 
+from src.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
-    def get_data_tansformation_object(self):
+    def get_data_transformer_object(self):
+        '''
+        This function si responsible for data trnasformation
+        
+        '''
         try:
-            numerical_columns=["writing_score","reading_score"]
+            numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
                 "gender",
                 "race_ethnicity",
@@ -112,8 +122,4 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e,sys)
-
-
-
-
 
